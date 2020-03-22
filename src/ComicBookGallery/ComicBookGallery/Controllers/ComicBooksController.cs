f83dabcde44ca5cd94e02594651fbccb6ecd2530
lines <lines@DESKTOP-6B8D4G5>
2020-03-22 10:53:14 -0700

    Update 123456

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,17 @@ namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        public ActionResult Detail()
+        {
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Sunday)
+            {
+                return Redirect("/");
+                
+            }
+
+            return Content("Hello from the ComicBooksController");
+
+          
+        }
     }
 }
